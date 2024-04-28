0a8915a32bcb90c597b3f867dfaddecab09d258c
Sayan <sayanmajumdar4444@gmail.com>
2024-04-28 23:15:42 +0530

    Env variable added

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,18 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/database';
 
+const apiId = process.env.REACT_APP_API_ID;
+const apiKey = process.env.REACT_APP_API_KEY;
+
+
 const firebaseConfig = {
-    "apiKey": "AIzaSyBoNyphp0cl_jxjD2ZR3gjcd3MY56zXbzY",
+    "apiKey": apiKey,
     "authDomain": "raspberrypidata-3a619.firebaseapp.com",
     "projectId": "raspberrypidata-3a619",
     "databaseURL" : "https://raspberrypidata-3a619-default-rtdb.firebaseio.com/",
     "storageBucket": "raspberrypidata-3a619.appspot.com",
     "messagingSenderId": "717931168998",
-    "appId": "1:717931168998:web:f5b82f2e710ccc3a1ca727",
+    "appId": apiId,
     "measurementId": "G-0CF8B985MH"
 };
 
